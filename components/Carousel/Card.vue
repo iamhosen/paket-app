8b0b071c41e14a14ac0e2dba7f72341c2f893d58
iamhosen <hamirhosseini3@gmail.com>
2024-08-18 01:58:27 +0330

    style: Update card number formatting in Carousel/Card.vue

--- components/Carousel/Card.vue
+++ components/Carousel/Card.vue
@@ -10,14 +10,14 @@
     :style="{ transform: transformString }"
     @click="onClick"
   >
-    <div class="w-full h-full p-8 flex flex-col gap-y-4">
-      <h3 class="text-xl">{{ card.name }}</h3>
-      <span class="font-xero ltr text-[1.4em] text-center tracking-wider">
+    <div class="w-full h-full p-4 flex flex-col gap-y-4">
+      <h3>{{ card.name }}</h3>
+      <span class="font-xero ltr text-center tracking-wider">
         {{ styledCardNumber }}
       </span>
       <span class="flex flex-col">
-        <p class="font-normal opacity-40">موجودی</p>
-        <p class="font-bold text-2xl tracking-widest">
+        <p class="font-normal text-xs opacity-40">موجودی</p>
+        <p class="font-bold text-sm tracking-widest">
           {{ toPersianNumberFormat(card.total_amount) }}
         </p>
       </span>
